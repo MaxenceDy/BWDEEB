9c32dd8f464612c95da2c000a099aea274a0ccab
Pithaya <gaelle.chalot@viacesi.fr>
2017-04-13 13:22:08 +0200

    marche bordel

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta http-equiv="content-type" content="text/html" charset="utf-8">
+    <title>BDE Exia Reims</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+
+  <body>
+    <header>
+        <img src="Images/Logo.png" alt="logo" id="logoHeader">
+        <h1 id="titreHeader">Site du BDE</h1>
+    </header>
+
+    <nav>
+
+    </nav>
+    
+    <footer>
+    </footer>
+    
+  </body>
+</html>
