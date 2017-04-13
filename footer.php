934635db8b3e73da47d5f5152662a93b54c23ab5
maxence Dufrenoy <maxence Dufrenoy>
2017-04-13 14:33:45 +0200

    test

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer>
 	
-	<a href="mention-legal.php">mention légale</a>
+	<a id="ml" href="mention-legal.php">Mention légale</a>
+	<a id="contact" href="contact.php">Contact</a>
 	
 	
 </footer>
