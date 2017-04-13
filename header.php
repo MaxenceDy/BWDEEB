d8212309320d54cf1e5cab495c688f31054ce6fe
Pithaya <gaelle.chalot@viacesi.fr>
2017-04-13 14:54:53 +0200

    yah

--- header.php
+++ header.php
@@ -1,4 +1,8 @@
 <header>
     <img src="Images/Logo.png" alt="logo" id="logoHeader">
     <h1 id="titreHeader">Site du BDE</h1>
+
+    <a href="Inscription.php">Inscription</a>
+    <a href="Connexion.php">Connexion</a>
+    
 </header>
