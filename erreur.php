6d7d28c389dc47402ad1f37bc30d9e84b6246a1d
maxence Dufrenoy <maxence Dufrenoy>
2017-04-13 22:35:50 +0200

    htaccess, erreur, other stuff

---
+++ erreur.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta http-equiv="content-type" content="text/html" charset="utf-8">
+    <title>BDE Exia Reims : connexion</title>
+    <link rel="stylesheet" type="text/css" href="header.css">
+    <link rel="stylesheet" type="text/css" href="login.css">
+  </head>
+
+  <body>
+
+  <?php include('header.php'); ?>
+    
+	<div id="wrapper">
+		<p> La page que vous rechercher n'existe pas.</p>
+	</div>
+
+  <?php include 'footer.php'; ?>
+	
+  </body>
+</html>
