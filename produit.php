d79d9678c81321e390c9edf5b75dc69fec69c6a0
Pithaya <gaelle.chalot@viacesi.fr>
2017-04-14 10:05:17 +0200

    ajout de page produit

---
+++ produit.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta http-equiv="content-type" content="text/html" charset="utf-8">
+    <title>BDE Exia Reims : connexion</title>
+    <link rel="stylesheet" type="text/css" href="header.css">
+    <link rel="stylesheet" type="text/css" href="produit.css">
+  </head>
+
+  <body>
+
+  <?php include('header.php'); ?>
+  
+  <div id="wrapper">
+    <div id="containerImg">
+	    <img src="Images/gourde.jpg">
+    </div>
+
+    <aside id="containerDsc">
+      <h1>Gourde Exia</h1>
+      <p>
+        Description :<br>
+        Magnifique gourde avec le logo de l'Exia pour partir en balade.
+      </p>
+    </aside>
+
+  </div>
+  
+  <?php include 'footer.php'; ?>
+	
+  </body>
+</html>
