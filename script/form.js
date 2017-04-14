cf0c6b22af7969189396b293b33261e0d3f14f7d
maxence Dufrenoy <maxence Dufrenoy>
2017-04-14 23:31:53 +0200

    commentaire js

--- script/form.js
+++ script/form.js
@@ -1,50 +1,26 @@
 window.onload = function() {
+	// au chargement de la page seul Avatar est afficher par défaut
 	document.getElementById("gest-perso").classList.add("Hide");
-	//document.getElementById("experience").classList.add("Hide");
-	//document.getElementById("loisirs").classList.add("Hide");
 	
+	// ajout des EventListener
 	document.getElementById("bt_avatar").addEventListener("click", ShowAvatar);
 	document.getElementById("bt_info").addEventListener("click", ShowInfo);
-	//document.getElementById("bt_exp").addEventListener("click", ShowExp);
-	//document.getElementById("bt_L").addEventListener("click", ShowLoisirs);
-	
 	
 };
 
+
+// cache la partie INFO pour laisser place à la partie Avatar
 function ShowAvatar(e){
-		e.preventDefault();
-		document.getElementById("gest-avatar").classList.remove("Hide");
-		
-		document.getElementById("gest-perso").classList.add("Hide");
-		//document.getElementById("experience").classList.add("Hide");
-		//document.getElementById("loisirs").classList.add("Hide");
-	}
+	e.preventDefault();
+	document.getElementById("gest-avatar").classList.remove("Hide");
 	
-	function ShowInfo(e){
-		e.preventDefault();
-		document.getElementById("gest-perso").classList.remove("Hide");
-		
-		document.getElementById("gest-avatar").classList.add("Hide");
-		//document.getElementById("experience").classList.add("Hide");
-		//document.getElementById("loisirs").classList.add("Hide");
-	}
+	document.getElementById("gest-perso").classList.add("Hide");
+}
 	
-	/*
-	function ShowExp(e){
-		e.preventDefault();
-		document.getElementById("experience").classList.remove("Hide");
-		
-		document.getElementById("competence").classList.add("Hide");
-		document.getElementById("formation").classList.add("Hide");
-		document.getElementById("loisirs").classList.add("Hide");
-	}
+// cache la partie Avatar pour laisser place à la partie INFO
+function ShowInfo(e){
+	e.preventDefault();
+	document.getElementById("gest-perso").classList.remove("Hide");
 	
-	function ShowLoisirs(e){
-		e.preventDefault();
-		document.getElementById("loisirs").classList.remove("Hide");
-		
-		document.getElementById("competence").classList.add("Hide");
-		document.getElementById("experience").classList.add("Hide");
-		document.getElementById("formation").classList.add("Hide");
-	}
-	*/
+	document.getElementById("gest-avatar").classList.add("Hide");
+}
