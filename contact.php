f139084218ccb3475e8c069a0a12f10a4f44ca60
Pithaya <gaelle.chalot@viacesi.fr>
2017-04-14 09:43:56 +0200

    Font

--- contact.php
+++ contact.php
@@ -30,7 +30,7 @@
 
     <div class="flex-container">
       <div class= "flex-item">
-        <h2>Chargé d'évenement</h2>
+        <h2>Chargé d'évènement</h2>
         <img src="Images/stephane1.png" alt="logo" id="Avatar">
         <p>Numéro de teléphone: <a href="tel:0652453333">06 52 45 90 67</a></p>
         <p>Mail: <a href="mailto:joshua.liaud@viacesi.fr">Envoyer un mail</a></p>
@@ -57,7 +57,7 @@
 
     <div class="flex-container">
       <div class= "flex-item">
-        <h2>Chargé d'évenement</h2>
+        <h2>Chargé d'évènement</h2>
         <img src="Images/dorian.jpg" alt="logo" id="Avatar">
         <p>Numéro de teléphone: <a href="tel:0652453333">06 52 45 90 67</a></p>
         <p>Mail: <a href="mailto:dorian.broutin@viacesi.fr">Envoyer un mail</a></p>
