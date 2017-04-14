f4078ef87b4812a64d6b4be42f009331c4be9561
jord08 <joshua.liaud@viacesi.fr>
2017-04-14 14:41:21 +0200

    mentions légales

--- activites.php
+++ activites.php
@@ -4,7 +4,7 @@
     <meta http-equiv="content-type" content="text/html" charset="utf-8">
     <title>BDE Exia Reims : connexion</title>
     <link rel="stylesheet" type="text/css" href="header.css">
-    <link rel="stylesheet" type="text/css" href="boutique.css">
+    <link rel="stylesheet" type="text/css" href="activites.css">
   </head>
 
   <body>
@@ -12,7 +12,59 @@
   <?php include('header.php'); ?>
     
 	<div id="wrapper">
-    
+    <h1>Activités à venir</h1>
+
+    <div id="activiteFutures">
+      <div class="container">
+        <img src="Images/foret.png">
+        <h2>Voyage en pleine nature</h2>
+        <p>12h</p>
+      </div>
+      <div class="container">
+        <img src="Images/mer.png">
+        <h2>Voyage en pleine mer</h2>
+        <p>12h</p>
+      </div>
+      <div class="container">
+        <img src="Images/desert.png">
+        <h2>Voyage en plein desert</h2>
+        <p>12h</p>
+      </div>
+    </div>
+
+    <h1>Toutes nos activites</h1>
+    <div id="activites">
+      <div class="container">
+        <img src="Images/foret.png">
+        <h2>Voyage en pleine nature</h2>
+        <p>12h</p>
+      </div>
+      <div class="container">
+        <img src="Images/mer.png">
+        <h2>Voyage en pleine mer</h2>
+        <p>12h</p>
+      </div>
+      <div class="container">
+        <img src="Images/desert.png">
+        <h2>Voyage en plein desert</h2>
+        <p>12h</p>
+      </div>
+      <div class="container">
+        <img src="Images/foret.png">
+        <h2>Voyage en pleine nature</h2>
+        <p>12h</p>
+      </div>
+      <div class="container">
+        <img src="Images/mer.png">
+        <h2>Voyage en pleine mer</h2>
+        <p>12h</p>
+      </div>
+      <div class="container">
+        <img src="Images/desert.png">
+        <h2>Voyage en plein desert</h2>
+        <p>12h</p>
+      </div>
+    </div>
   </div>
 
   <?php include 'footer.php'; ?>
