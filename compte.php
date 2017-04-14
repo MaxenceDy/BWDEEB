1f279cffd24e57ae59b7bfe55247a8d728c3c41f
maxence Dufrenoy <maxence Dufrenoy>
2017-04-14 17:25:42 +0200

    modif compte

--- compte.php
+++ compte.php
@@ -11,56 +11,70 @@
 
   <?php include('header.php'); ?>
     
-	<div id=wrapper>
-	
-	
-		<!-- GESTION DE L'AVATAR -->
-		<div id="gest-avatar">
-			<form method="POST" action="upload.php" enctype="multipart/form-data">
+		
+		
+		<div id=wrapper>
+			
+	<div id=wrapper2>
+		
+		<!-- NAVIGATION -->
+			<div id="nav">
+				<nav id="compte">
+					<ul>
+						<li><a href="index.php">Avatar</a></li>
+						<li><a href="activites.php">info perso</a></li>        
+					</ul>        
+				</nav>
+			</div>
+			
+			<!-- GESTION DE L'AVATAR -->
+			<div id="gest-avatar">
+				<form method="POST" action="upload.php" enctype="multipart/form-data">
+					<div class="form">
+						
+						<!-- On limite le fichier à 100Ko -->
+						<input type="hidden" name="MAX_FILE_SIZE" value="100000">
+						
+						<br /><label for="email">Fichier : </label> <br />
+						<input type="file" name="avatar"> <br />
+						
+						<input type="submit" name="envoyer" value="Envoyer le fichier">
+					</div>
+				</form>
+				
+			</div>
+			
+			<!-- GESTION DES INFO PERSO -->
+			<div id="gest-perso">
+				<form id="gest-perso" method="POST" action="#">
 				<div class="form">
 					
-					<!-- On limite le fichier à 100Ko -->
-					<input type="hidden" name="MAX_FILE_SIZE" value="100000">
+					<br /><label for="nom">Nom</label> <br />
+					<input type="text" name="nom" id="nom" /> <br />
 					
-					<br /><label for="email">Fichier : </label> <br />
-					<input type="file" name="avatar"> <br />
+					<label for="prenom">Prénom</label> <br />
+					<input type="text" name="prenom" id="prenom" /> <br />
 					
-					<input type="submit" name="envoyer" value="Envoyer le fichier">
+					<label for="dateN">Date de naissance</label> <br />
+					<input type="date" name="dateN" id="dateN" /> <br />
+					
+					<br /><label for="ville">Ville</label> <br />
+					<input type="text" name="ville" id="ville" /> <br />
+					
+					<label for="adresse">Adresse</label> <br />
+					<input type="text" name="adresse" id="adresse" /> <br />
+					
+					<label for="code">Code postal</label> <br />
+					<input type="text" name="code" id="code" /> <br />
+					
+					<input type="submit" value="enregistrer" />
 				</div>
 			</form>
-			
-		</div>
-		
-		<!-- GESTION DES INFO PERSO -->
-		<div id="gest-perso">
-			<form id="gest-perso" method="POST" action="#">
-			<div class="form">
 				
-				<br /><label for="nom">Nom</label> <br />
-				<input type="text" name="nom" id="nom" /> <br />
-				
-				<label for="prenom">Prénom</label> <br />
-				<input type="text" name="prenom" id="prenom" /> <br />
-				
-				<label for="dateN">Date de naissance</label> <br />
-				<input type="date" name="dateN" id="dateN" /> <br />
-				
-				<br /><label for="ville">Ville</label> <br />
-				<input type="text" name="ville" id="ville" /> <br />
-				
-				<label for="adresse">Adresse</label> <br />
-				<input type="text" name="adresse" id="adresse" /> <br />
-				
-				<label for="code">Code postal</label> <br />
-				<input type="text" name="code" id="code" /> <br />
-				
-				<input type="submit" value="enregistrer" />
 			</div>
-		</form>
 			
-		</div>
-		
-		
+			
+	  </div>
   </div>
 
   <?php include 'footer.php'; ?>
