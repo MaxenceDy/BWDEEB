d564026257a573870faea4db96cf74445cf5ff86
jord08 <joshua.liaud@viacesi.fr>
2017-04-15 12:02:36 +0200

    commentaire-photo-underconstruction

---
+++ commentaire-photo.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta http-equiv="content-type" content="text/html" charset="utf-8">
+    <title>BDE Exia Reims : connexion</title>
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/commentaire-photo.css">
+  </head>
+
+  <body>
+
+  <?php include('header.php'); ?>
+    
+	<div id="wrapper">
+    <h1>Commentaire Photo</h1>
+
+    <div id="PhotoCom">
+      <div class="container">
+        <img src="Images/foret.png">
+        <br>
+        <a href="" onclick=""><img src="Images/poucebleu.jpg" alt="j'aime" id="jaime"></a>
+      </div>
+      <div class="container" id="ComOut">
+        <div id="ComIn">
+          <h4>LIAUD Joshua<br></h4>
+          <br>
+          <hr></hr>
+          <p>Je commente une première fois</p>
+        </div>
+        <div id="ComIn">
+          <h4>LIAUD Joshua<br></h4>
+          <br>
+          <hr></hr>
+          <p>Je commente une deuxième fois</p>
+        </div>
+        <div id="ComIn">
+          <h4>LIAUD Joshua<br></h4>
+          <br>
+          <hr></hr>
+          <p>Je commente une troisième fois</p>
+        </div>
+        <div id="ComIn">
+          <h4>LIAUD Joshua<br></h4>
+          <br>
+          <hr></hr>
+          <p>Je commente une cinquième fois</p>
+        </div>
+        <div id="ComIn">
+          <h4>LIAUD Joshua<br></h4>
+          <br>
+          <hr></hr>
+          <p>Je commente une sixième fois bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+        </div>
+        <br>
+      </div>
+    </div>
+  </div>
+
+  <?php include 'footer.php'; ?>
+	
+  </body>
+</html>
