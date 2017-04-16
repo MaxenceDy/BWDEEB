493c32d52ef02d0633b7143aef40f39a4f9436ef
maxence Dufrenoy <maxence Dufrenoy>
2017-04-16 02:53:46 +0200

    id regenerate

--- verification.php
+++ verification.php
@@ -2,10 +2,12 @@
 	session_start();
 	
 	var_dump($_SESSION);
+	var_dump(session_id());
 	
 	if(!empty($_SESSION))
 	{
-		$_SESSION['connecte'] = true;;
+		$_SESSION['connecte'] = true;
+		session_regenerate_id();
 	}
 
 ?>
