59e69a85fc7f414255f6af57c3593fc8ab9b78ae
maxence Dufrenoy <maxence Dufrenoy>
2017-04-16 17:30:35 +0200

    expiration de session (bug a reparer restant pour index.php)

--- verification.php
+++ verification.php
@@ -10,7 +10,7 @@
 		session_regenerate_id();
 		
 		if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 10)) {
-			// last request was more than 30 minutes ago
+			// last request was more than 10 sec ago
 			session_unset();     // unset $_SESSION variable for the run-time 
 			session_destroy();   // destroy session data in storage
 		}
