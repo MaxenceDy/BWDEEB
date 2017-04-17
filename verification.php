2d9e8fc5a02189850d46f503da594e935a658cd1
maxence Dufrenoy <maxence Dufrenoy>
2017-04-17 23:08:06 +0200

    form js modifier

--- verification.php
+++ verification.php
@@ -9,7 +9,7 @@
 		$_SESSION['connecte'] = true;
 		session_regenerate_id();
 		
-		if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 300)) {
+		if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 7200)) {
 			// last request was more than 10 sec ago
 			session_unset();     // unset $_SESSION variable for the run-time 
 			session_destroy();   // destroy session data in storage
